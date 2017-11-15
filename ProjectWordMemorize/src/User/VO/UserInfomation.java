78cd2e59282a74831e358d8c047d45fbb1cb6dc4
eodud4106 <eodud4106@naver.com>
2017-11-15 20:56:08 +0900

    UserInfo

--- ProjectWordMemorize/src/User/VO/UserInfomation.java
+++ ProjectWordMemorize/src/User/VO/UserInfomation.java
@@ -1,8 +1,69 @@
 package User.VO;
 
 public class UserInfomation {
+	private int accountnumber;
 	private String userID;
 	private String password;
+	private String createdate;
 	
+	public UserInfomation() {}
 
+	public UserInfomation(String userID, String password) {
+		this.userID = userID;
+		this.password = password;
+	}
+	public UserInfomation(int accountnumber, String userID, String password, String createdate) {
+		this.accountnumber = accountnumber;
+		this.userID = userID;
+		this.password = password;
+		this.createdate = createdate;
+	}
+
+	public int getAccountnumber() {
+		return accountnumber;
+	}
+
+	public void setAccountnumber(int accountnumber) {
+		this.accountnumber = accountnumber;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCreatedate() {
+		return createdate;
+	}
+
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+
+	@Override
+	public String toString() {
+		String s = "会員番号 : "+accountnumber+", アイディー : "+userID
+				+", 暗証番号 : "+password+", 生成日 : "+createdate;
+		return s;
+	}
+	
+	
+	
+	
+	
+	
 }
+
+
+
