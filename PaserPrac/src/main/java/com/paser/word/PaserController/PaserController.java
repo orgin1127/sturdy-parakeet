220ff6b95e3c2313b338884f5bd535415def606e
origin1127 <kwanhun21@gmail.com>
2018-01-25 20:52:29 +0900

    1/25 Commit

---
+++ PaserPrac/src/main/java/com/paser/word/PaserController/PaserController.java
@@ -0,0 +1,53 @@
+package com.paser.word.PaserController;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PaserController  {
+	Logger logger = LoggerFactory.getLogger(PaserController.class);
+	
+	@RequestMapping(value="startPaser", method = RequestMethod.GET)
+	public String startPaser(Model model) throws Exception {
+		
+		int[] lastPage = {73, 60,35,24,14};
+		int[] levels = {1, 2, 3, 4, 5};
+		String output = "";
+		
+		for (int i  = 0; i  < levels.length; i ++) {
+			output += levels[i]+ "급";
+			output += System.getProperty("line.separator");
+			for (int y = 1; y <= lastPage[i]; y++) {
+				//페이지 번호를 입력
+				String url = "http://jpdic.naver.com/jlpt/level-" + levels[i] + "/parts-0/p" + y +".nhn"; 
+				Document doc = Jsoup.connect(url).get();
+				String selector = "span.jp";
+				String selector2 = "span.bot_txt";
+				String selector3 = "a";
+				Elements el = doc.select(selector);
+				Elements el2 = doc.select(selector2);
+				
+				for (Element e : el) {
+					output += e.text();
+					output += System.getProperty("line.separator");
+				}
+				for (Element e2 : el2) {
+					output += e2.text();
+					output += System.getProperty("line.separator");
+				}
+			}
+			output += System.getProperty("line.separator");
+		}
+		
+		model.addAttribute("result", output);
+		return "./JSP/pasingResult";
+	}
+}
---
+++ PaserPrac/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+
+<P>  The time on the server is ${serverTime}. </P>
+
+<p><a href="startPaser">GO TO PASER</a></p>
+</body>
+</html>
