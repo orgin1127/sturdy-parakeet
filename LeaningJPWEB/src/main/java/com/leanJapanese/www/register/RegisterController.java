d36f5999725a4599d078286009392284264d74e0
user <user@192.168.1.15>
2018-01-27 01:44:38 +0900

    1/27 Commit2

--- LeaningJPWEB/src/main/java/com/leanJapanese/www/register/RegisterController.java
+++ LeaningJPWEB/src/main/java/com/leanJapanese/www/register/RegisterController.java
@@ -1,8 +1,24 @@
 package com.leanJapanese.www.register;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
-
+	Logger logger = LoggerFactory.getLogger(RegisterController.class);
+	
+	@RequestMapping(value = "register", method = RequestMethod.GET)
+	public String register() {
+		return "";
+	}
+	
+	@RequestMapping(value = "register", method = RequestMethod.POST)
+	public String register(Model model) {
+		return "";
+	}
 }
